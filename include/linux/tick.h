007693691e41550e383904f2e9ca0ca7f4a52e14
NotZeetaa <rodrigo2005contente@gmail.com>
2023-06-08 16:00:30 +0000

    Merge branch 'android-4.19-stable' of https://android.googlesource.com/kernel/common into rebase
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- include/linux/tick.h
+++ include/linux/tick.h
@@ -197,6 +197,7 @@ extern void tick_nohz_dep_set_signal(struct signal_struct *signal,
 				     enum tick_dep_bits bit);
 extern void tick_nohz_dep_clear_signal(struct signal_struct *signal,
 				       enum tick_dep_bits bit);
+extern bool tick_nohz_cpu_hotpluggable(unsigned int cpu);
 
 /*
  * The below are tick_nohz_[set,clear]_dep() wrappers that optimize off-cases
@@ -258,6 +259,8 @@ extern void __init tick_nohz_full_setup(cpumask_var_t cpumask);
 static inline bool tick_nohz_full_enabled(void) { return false; }
 static inline bool tick_nohz_full_cpu(int cpu) { return false; }
 static inline void tick_nohz_full_add_cpus_to(struct cpumask *mask) { }
+
+static inline bool tick_nohz_cpu_hotpluggable(unsigned int cpu) { return true; }
 
 static inline void tick_dep_set(enum tick_dep_bits bit) { }
 static inline void tick_dep_clear(enum tick_dep_bits bit) { }
