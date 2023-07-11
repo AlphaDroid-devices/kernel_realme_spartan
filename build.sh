b84f097da4409fe34e2d01ae717aaf03266683aa
amackpro <prajwal.kumar008@gmail.com>
2023-07-11 14:34:27 +0530

    Restructure build tool and rename kernel

--- build.sh
+++ build.sh
@@ -1,24 +1,107 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
+# shellcheck disable=SC2199
+# shellcheck source=/dev/null
 #
-# Clone proton-clang toolchain if needed
+# Copyright (C) 2020-22 UtsavBalar1231 <utsavbalar1231@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+
+KBUILD_COMPILER_STRING=$($HOME/tc/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+KBUILD_LINKER_STRING=$($HOME/tc/clang/bin/ld.lld --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' | sed 's/(compatible with [^)]*)//')
+export KBUILD_COMPILER_STRING
+export KBUILD_LINKER_STRING
+#
+# Enviromental Variables
 #
 
-KERNEL_DEFCONFIG=vendor/sm8250_defconfig
-DIR=$PWD
-export ARCH=arm64
-export SUBARCH=arm64
-export CLANG_PATH="~/toolchains/bin"
-export PATH="$CLANG_PATH:$PATH"
-export CROSS_COMPILE=aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-export KBUILD_BUILD_USER=Amog
-export KBUILD_BUILD_HOST=Us
+DATE=$(date '+%Y%m%d-%H%M')
 
-echo
-echo "Kernel is going to be built using $KERNEL_DEFCONFIG"
-echo
+DEVICE="RE54E4L1"
 
-make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1 O=out $KERNEL_DEFCONFIG
+# Set our directory
+OUT_DIR=out/
+VERSION="MiNaZuKi-${DEVICE^^}-AOSP-${DATE}"
 
-make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1 O=out -j$(nproc --all)
+# Export Zip name
+export ZIPNAME="${VERSION}.zip"
+
+# How much kebabs we need? Kanged from @raphielscape :)
+if [[ -z "${KEBABS}" ]]; then
+    COUNT="$(grep -c '^processor' /proc/cpuinfo)"
+    export KEBABS="$((COUNT + 2))"
+fi
+
+echo "Jobs: ${KEBABS}"
+
+ARGS="ARCH=arm64 \
+O=${OUT_DIR} \
+CC=clang \
+LD=ld.lld \
+AR=llvm-ar \
+NM=llvm-nm \
+OBJCOPY=llvm-objcopy \
+OBJDUMP=llvm-objdump \
+STRIP=llvm-strip \
+CLANG_TRIPLE=aarch64-linux-gnu- \
+CROSS_COMPILE=aarch64-linux-gnu- \
+CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
+-j${KEBABS}"
+
+dts_source=arch/arm64/boot/dts/vendor
+
+START=$(date +"%s")
+
+# Set compiler Path
+export PATH="$HOME/tc/clang/bin:$PATH"
+export LD_LIBRARY_PATH=${HOME}/tc/clang/bin:$LD_LIBRARY_PATH
+
+# Make defconfig
+make -j${KEBABS} ${ARGS} vendor/sm8250_defconfig
+
+# Make olddefconfig
+cd ${OUT_DIR} || exit
+make -j${KEBABS} ${ARGS} CC="ccache clang" HOSTCC="ccache gcc" HOSTCXX="cache g++" olddefconfig
+cd ../ || exit
+
+make -j${KEBABS} ${ARGS} CC="ccache clang" HOSTCC="ccache gcc" HOSTCXX="ccache g++" 2>&1 | tee build.log
+
+#remove KSU from source after compiling
+git checkout drivers/Makefile &>/dev/null
+rm -rf KernelSU
+rm -rf drivers/kernelsu
+
+find out/${dts_source} -name '*.dtb' -exec cat {} + >out/arch/arm64/boot/dtb
+
+END=$(date +"%s")
+DIFF=$((END - START))
+zipname="$VERSION.zip"
+if [ -f "out/arch/arm64/boot/Image" ] && [ -f "out/arch/arm64/boot/dtbo.img" ] && [ -f "out/arch/arm64/boot/dtb" ]; then
+	git clone -q https://github.com/amackpro/AnyKernel3 -b $DEVICE
+	cp out/arch/arm64/boot/Image AnyKernel3
+	cp out/arch/arm64/boot/dtb AnyKernel3
+	cp out/arch/arm64/boot/dtbo.img AnyKernel3
+	rm -f *zip
+	cd AnyKernel3
+	sed -i "s/is_slot_device=0/is_slot_device=auto/g" anykernel.sh
+	zip -r9 "../${zipname}" * -x '*.git*' README.md *placeholder >> /dev/null
+	cd ..
+	rm -rf AnyKernel3
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+	echo ""
+	echo -e ${zipname} " is ready!"
+	echo ""
+else
+	echo -e "\n Compilation Failed!"
+fi
